ba01fc5c62c664ebc0d8083149d010ddc3098f8c
HarshJain-29 <git29harsh@gmail.com>
2025-07-03 21:21:35 +0530

    commit somtihing1

--- src/App.js
+++ src/App.js
@@ -635,7 +635,7 @@ const App = () => {
                                                 <img
                                                     src={currentOpenCard.media}
                                                     alt={currentOpenCard.title}
-                                                    className="rounded-lg shadow-md w-full object-contain border-2 border-rose-300 mx-auto"
+                                                    className="rounded-lg shadow-md w-[80%] max-w-md object-contain border-2 border-rose-300 mx-auto"
                                                 />
                                             )}
                                         </div>
